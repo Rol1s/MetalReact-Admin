c9d7344e4eeeb03d5744dc7d9cc20ac4862e67c4
Rol1s <rol1s@example.com>
2025-11-24 10:40:54 +0300

    fix: Update base URL for MetalReact-Admin repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MetalReact_V2/',
+  base: '/MetalReact-Admin/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
